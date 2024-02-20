b87a71c0c6e84633f7d057e7fff1599a934220e4
JordanDestin <destin.jord@gmail.com>
2024-02-20 22:24:59 +0100

    up

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ from urllib.parse import urljoin
 
 BASE_URL = "https://www.marmiton.org/"
 
-def get_number_of_pages(url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        pagination = soup.find("nav", class_="af-pagination")
-        if not pagination:
-            return 0
-        number_pages = pagination.find_all('a')
-        return max([int(page.get_text()) for page in number_pages if page.get_text().isdigit()])
-
-
 def get_all_categories(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -23,9 +13,47 @@ def get_all_categories(url):
     categories_url = [category["href"] for category in categories]
     full_url_categories = [urljoin(BASE_URL, category) for category in categories_url]
     
-    return full_url_categories
-   ## pprint(full_url_categories)
+    urls_categorie = [url for url in full_url_categories if "categorie" in url]
+    urls_modifiees = [url.replace("?rcp=0", "") for url in urls_categorie]
+    return urls_modifiees[0:1]
+    #pprint(urls_modifiees[0])
+
+
+def get_number_of_pages(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    pagination = soup.find("nav", class_="af-pagination")
+    if not pagination:
+        return 0
+    number_pages = pagination.find_all('a')
+    return max([int(page.get_text()) for page in number_pages if page.get_text().isdigit()])
+
+
+
          
+
+def get_all_recipes_name(url):
+    list_recipe_names = []
+  
+    list_categories = get_all_categories(url)
+    pprint(list_categories)
+    for category in list_categories:
+        number_pages = get_number_of_pages(category)
+        pprint(number_pages)
+        for i in range(1, number_pages+1):
+            page_url = category + "/"+ str(i)
+            #pprint(page_url)
+            response = requests.get(page_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            recipes_name = soup.find_all("h4", class_="recipe-card__title")
+            pprint(recipes_name)
+            """for recipe in recipes_name:
+                list_recipe_names.append(recipe.get_text().strip())"""
+    #pprint(list_recipe_names)
+   
+
+        
+   
 
 def main():
     with requests.Session() as session:
@@ -36,9 +64,10 @@ def main():
             print(f'Error{e}')
             raise requests.exceptions.RequestException from e
 
-        list_categories = get_all_categories(BASE_URL)
-        number_of_pages = get_number_of_pages(list_categories[0]) 
-        get_all_categories(BASE_URL)
+        #list_categories = get_all_categories(BASE_URL)
+        #number_of_pages = get_number_of_pages(list_categories) 
+        #get_all_categories(BASE_URL)
+        get_all_recipes_name(BASE_URL)
 
         """categories = soup.find("li",class_="mrtn-header-menu-title").find_all("a", class_="mrtn-header-subitem-link")
         #categories = soup.select("li.mrtn-header-menu-title ul li a.mrtn-header-subitem-link")
