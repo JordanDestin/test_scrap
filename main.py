a57c279604d3821b12570d6d79b9949d432facd3
JordanDestin <destin.jord@gmail.com>
2024-02-29 12:08:41 +0100

    up

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 from urllib.parse import urljoin
+from selectolax.parser import HTMLParser
+from loguru import logger
+import sys
 
+logger.remove() 
+logger.add("logs.log", level="WARNING", rotation="500kb")
+logger.add(sys.stderr, level="INFO")
 
 BASE_URL = "https://www.marmiton.org/"
 
+
+
 def get_all_categories(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    tree = HTMLParser(response.text)
+    categories = tree.css("li.mrtn-header-menu-title ul li a.mrtn-header-subitem-link")
+
+    
+    """soup = BeautifulSoup(response.text, 'html.parser')
     categories = soup.find("li",class_="mrtn-header-menu-title").find_all("a", class_="mrtn-header-subitem-link")
     categories_url = [category["href"] for category in categories]
     full_url_categories = [urljoin(BASE_URL, category) for category in categories_url]
     
     urls_categorie = [url for url in full_url_categories if "categorie" in url]
-    urls_modifiees = [url.replace("?rcp=0", "") for url in urls_categorie]
-    return urls_modifiees[0:1]
-    #pprint(urls_modifiees[0])
+    urls_modifiees = [url.replace("?rcp=0", "") for url in urls_categorie]"""
+    #return urls_modifiees[0:1]
+    pprint(categories)
 
 
 def get_number_of_pages(url):
@@ -29,10 +41,12 @@ def get_number_of_pages(url):
     return max([int(page.get_text()) for page in number_pages if page.get_text().isdigit()])
 
 
+def get_all_recipes_urls_by_category(url):
+    pass
 
          
 
-def get_all_recipes_name(url):
+def get_all_recipes_name_by_category(url):
     list_recipe_names = []
   
     list_categories = get_all_categories(url)
@@ -52,7 +66,8 @@ def get_all_recipes_name(url):
     #pprint(list_recipe_names)
    
 
-        
+def get_number_star_recipe(url):
+    pass      
    
 
 def main():
@@ -66,8 +81,8 @@ def main():
 
         #list_categories = get_all_categories(BASE_URL)
         #number_of_pages = get_number_of_pages(list_categories) 
-        #get_all_categories(BASE_URL)
-        get_all_recipes_name(BASE_URL)
+        get_all_categories(BASE_URL)
+        #get_all_recipes_name_by_category(BASE_URL)
 
         """categories = soup.find("li",class_="mrtn-header-menu-title").find_all("a", class_="mrtn-header-subitem-link")
         #categories = soup.select("li.mrtn-header-menu-title ul li a.mrtn-header-subitem-link")
